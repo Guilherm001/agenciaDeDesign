fa12fa9d74b09641c1a94a009ba311c087afbbcf
Guilherme <guilherm.rodrigues@outlook.com>
2025-03-12 19:54:12 -0300

    cards

--- src/app/components/Card/index.jsx
+++ src/app/components/Card/index.jsx
@@ -7,9 +7,10 @@ const Card = () => {
     return (
         <div className={Styles.card}>
             <div className={Styles.card_header}>
-                <h2>Web Designer</h2>
-                <h3>Pied Piper StartUp.</h3>
                 <p>JUNHO 2012 - 2016</p>
+                <h2>Web Designer</h2>
+                <p>Pied Piper StartUp.</p>
+                
             </div>
             <div className={Styles.card_body}>
                 <p>Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores</p>
